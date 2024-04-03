138620eee3deb009ae050fef0971161b7001c625
Caleb Mwasikira <calebmwasikira@gmail.com>
2024-04-03 20:15:05 +0300

    Dynamic programming on fibonacci

--- src/main.v
+++ src/main.v
@@ -1,44 +1,52 @@
 module main
 
-import data_structures as ds
+fn iterative_fib(number int) !i64 {
+	if number < 0 {
+		return error("cannot calculate fib of negative numbers")
+	}
+
+	if number == 0 || number == 1 {
+		return number
+	} else {
+		mut prev := i64(1)
+		mut prev2 := i64(0)
+		mut result := i64(prev + prev2)
+
+		for i := 2; i < number; i++ {
+			prev2 = prev
+			prev = result
+			result = prev + prev2
+		}
+
+		return result
+	}
+}
+
+fn recursive_fib(number int, mut memo map[int]i64) !i64 {
+	if number < 0 {
+		return error("cannot calculate fib of negative numbers")
+	}
+
+	if number in memo {
+		return memo[number]
+	}
+
+	if number == 0 || number == 1 {
+		return number
+	} else {
+		result := recursive_fib(number - 1, mut memo)! + recursive_fib(number - 2, mut memo)!
+		memo[number] = result
+		return result
+	}
+}
 
 fn main() {
-	prime_numbers := [1, 2, 3]
+	index := 100
+	// it_result := iterative_fib(index)!
 
-	mut ll := ds.LinkedList.from(...prime_numbers)!
-	println('begin linked list: ${ll.repr()}\n')
+	mut memo := map[int]i64{}
+	rec_result := recursive_fib(index, mut memo)!
 
-	odd_numbers := [9, 15]
-	ll.insert_at_end(...odd_numbers)
-	println('after insert_at_end ${odd_numbers}:\n ${ll.repr()}\n')
-
-	even_numbers := [4, 6, 8]
-	ll.insert_at_middle(1, ...even_numbers)!
-	println('after insert_at_middle ${even_numbers}:\n ${ll.repr()}\n')
-
-	ll.delete_head_node()!
-	println('after delete_head_node:\n ${ll.repr()}\n')
-
-	ll.delete_tail_node()!
-	println('after delete_tail_node:\n ${ll.repr()}\n')
-
-	index := 2
-	ll.delete_from_middle(index)!
-	println('after deleting index ${index}:\n ${ll.repr()}\n')
-
-	num_items := 2
-	ll.delete_n_items_from_middle(index, num_items)!
-	println('after deleting ${num_items} items starting at index ${index}:\n ${ll.repr()}\n')
-
-	data := 6
-	println('deleting node with data item ${data}...')
-	ll.delete_node(data) or {
-		eprintln(err)
-		println(ll.repr())
-
-		exit(1)
-	}
-	println(ll.repr())
-
-	println("final size of linked list: ${ll.size()}\n")
+	// println("iterative fib(${index}) = ${it_result}")
+	println("recursive fib(${index}) = ${rec_result}")
 }
