da02fbaca46fd612fe52404e94e5f358fc6fa532
Caleb Mwasikira <calebmwasikira@gmail.com>
2024-04-16 14:40:11 +0300

    Code refactor

--- src/main.v
+++ src/main.v
@@ -1,52 +1,19 @@
 module main
 
-fn iterative_fib(number int) !i64 {
-	if number < 0 {
-		return error("cannot calculate fib of negative numbers")
-	}
-
-	if number == 0 || number == 1 {
-		return number
-	} else {
-		mut prev := i64(1)
-		mut prev2 := i64(0)
-		mut result := i64(prev + prev2)
-
-		for i := 2; i < number; i++ {
-			prev2 = prev
-			prev = result
-			result = prev + prev2
-		}
-
-		return result
-	}
-}
-
-fn recursive_fib(number int, mut memo map[int]i64) !i64 {
-	if number < 0 {
-		return error("cannot calculate fib of negative numbers")
-	}
-
-	if number in memo {
-		return memo[number]
-	}
-
-	if number == 0 || number == 1 {
-		return number
-	} else {
-		result := recursive_fib(number - 1, mut memo)! + recursive_fib(number - 2, mut memo)!
-		memo[number] = result
-		return result
-	}
-}
+import data_structures as ds
 
 fn main() {
-	index := 100
-	// it_result := iterative_fib(index)!
+	prime_numbers := [1, 2, 3]
+	mut cll := ds.CircularLinkedList.from(...prime_numbers) or {
+		eprintln(err)
+		exit(1)
+	}
 
-	mut memo := map[int]i64{}
-	rec_result := recursive_fib(index, mut memo)!
+	println('initial: ${prime_numbers}\n ${cll.repr()}\n')
+	println('size: ${cll.size}\n')
 
-	// println("iterative fib(${index}) = ${it_result}")
-	println("recursive fib(${index}) = ${rec_result}")
+	odd_numbers := [11, 13, 15]
+	cll.insert_at_beginning(...odd_numbers)
+	println('after insert_at_beginning: ${odd_numbers}\n ${cll.repr()}')
+	println('size: ${cll.size}\n')
 }
